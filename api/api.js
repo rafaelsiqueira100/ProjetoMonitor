ee08f51de262764121f800aac8039fc5f60632aa
rafaelsiqueira100 <rafaelsiqueira100@gmail.com>
2018-05-10 09:24:14 -0300

    Atualizado estética api

--- api/api.js
+++ api/api.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const app = express();         
 const bodyParser = require('body-parser');
-const port = 50169; //porta padrão
+const porta = 50169; //porta padrão
 const sql = require('tedious');
 var datetime = require('node-datetime');
-//const connStr = "Server=Regulus;Database=BD16189;User Id=16189;Password=professorageraldina;";
-var Connection = require('tedious').Connection;
+//const connStr = "Server=Regulus;vetorHorariosbase=BD16189;User Id=16189;Password=professorageraldina;";
+var conexao = require('tedious').Connection;
 var TYPES = require('tedious').TYPES;
   var config = {
     userName: 'BD16189',
@@ -14,10 +14,10 @@ var TYPES = require('tedious').TYPES;
     server: 'Regulus',
   };
 
-  var connection = new Connection(config);
-    var data = [];
+  var conexao = new conexao(config);
+    var vetorHorarios = [];
 //fazendo a conexão global
- connection.on('connect', function(err) {
+ conexao.on('connect', function(err) {
     // If no error, then good to go...
       //abrir server
     }
@@ -25,30 +25,27 @@ var TYPES = require('tedious').TYPES;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-const router = express.Router();
-router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
-app.use('/', router);
+const rota = express.router();
+rota.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+app.use('/', rota);
 
 var Request = require('tedious').Request;
-router.get('/GetHorarios/:ds&:cod', (req, res) =>{
-    console.log(req.params.cod)  ; 
-    console.log(req.params.ds)  ; 
+rota.get('/GetHorarios/:ds&:cod', (req, res) =>{
+    
     if(req.params.ds && req.params.cod){
         var sql = 'selectHorario_sp';
     request = new Request(sql, function(err, rowCount) {
       if (err) {
         console.log(err);
       } else {
-        console.log(rowCount + ' rows');
-        console.log("3:"+data);
-        res.json(data);
-          data = [];
+        
+        res.json(vetorHorarios);
+          vetorHorarios = [];
       }
          
     });
         
         request.on('row', function(columns) {
-            console.log('entrou no loop');
             var hI = columns[0].value;
             var hF = columns[1].value;
             hI = String(hI);
@@ -65,25 +62,19 @@ router.get('/GetHorarios/:ds&:cod', (req, res) =>{
                 hF = '0'+ hF;
             sI = hI + sI.substring(2,8);
             sF = hF + sF.substring(2,8);
-      data.push({
+      vetorHorarios.push({
           'horarioInicio': sI,
           'horarioFim': sF
       })
-            console.log(sF);
-           //.format('H:M:S')
-            console.log(sI);
         
     })
     
-    console.log(req.params.ds)  ; 
-    console.log(req.params.cod)  ; 
-    console.log("2: "+data);
     request.addParameter('diaSemana', TYPES.Int, req.params.ds);
     request.addParameter('codMonitor', TYPES.Int, req.params.cod);
-    connection.callProcedure(request);
+    conexao.callProcedure(request);
     
     }
 })
 
-    app.listen(port);
+    app.listen(porta);
     console.log('A API está no ar');
